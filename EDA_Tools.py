1ec3f1dcc7102819d27f606de3adf54babd349e4
Adrian Santos <apsantos8@up.edu.ph>
2022-11-09 10:47:20 +0800

    Update count_col_values

--- EDA_Tools.py
+++ EDA_Tools.py
@@ -9,5 +9,5 @@ def describe_df(data_frame, head=False, info=False, describe=False):
 def count_col_value(data_frame, column_name, withPercentage=False):
     data_frame[column_name].value_counts()
     if withPercentage:
-        data_frame[column_name].value_counts(normalize)*100
+        data_frame[column_name].value_counts(normalize=True)*100
 
