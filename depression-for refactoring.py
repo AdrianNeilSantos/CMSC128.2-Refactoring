31d16c8c838402c09c24e2658599fe9ff3a9e1d1
Adrian Santos <apsantos8@up.edu.ph>
2022-11-09 09:38:51 +0800

    Removed error

--- depression-for refactoring.py
+++ depression-for refactoring.py
@@ -54,7 +54,7 @@ df = pd.read_csv("Depression.csv")
 df.head()
 df.info()
 df.describe()
-
+ 
 #to count how many have and do not have heart disease 
 df['DEPRESSED'].value_counts()
 
@@ -198,8 +198,8 @@ y_test.value_counts()
 #DECISION TREE Hyperparameter Tuning
 model = DecisionTreeClassifier()
 parameters = {'splitter' : ['best', 'random'],
-              'criterion' : ['gini', 'entropy'],
-              'max_features': ['log2', 'sqrt','auto'],
+              'criterion' : ['gini', 'entropy'], #removed 'auto': causes warning
+              'max_features': ['log2', 'sqrt'],
               'max_depth': [2, 3, 5, 10, 17],
               'min_samples_split': [2, 3, 5, 7, 9],
               'min_samples_leaf': [1,5,8,11], 
